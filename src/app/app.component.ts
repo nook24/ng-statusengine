b35d7a37f21fe767cb67f147194c8989b6716e84
nook24 <info@nook24.eu>
2024-03-27 18:37:05 +0100

    Basic page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatSlideToggle} from '@angular/material/slide-toggle'
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, MatSlideToggle, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
