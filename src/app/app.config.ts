905fa9a53f8024703ba0d8f6a189d4c2ba140a64
nook24 <info@nook24.eu>
2024-03-27 19:59:28 +0100

    Add proxy and first request

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {PROXY_PATH} from "./tokens/proxy-path.token";
+
+import { provideHttpClient } from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    { provide: PROXY_PATH, useValue: '/api' },
+    provideHttpClient(),
+    provideRouter(routes),
+    provideAnimationsAsync()
+  ]
 };
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DashboardService } from './dashboard.service';
+import { DashboardRoot } from './dashboard.interface';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatGridListModule,
+    DecimalPipe,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+
+  private subscriptions: Subscription = new Subscription();
+  private DashboardService = inject(DashboardService);
+  public dashboard!: DashboardRoot;
+
+  public hostColumns = ['State', 'Count', 'Percentage'];
+
+
+  constructor() {
+    this.loadDashboard();
+  }
+
+  loadDashboard() {
+    this.subscriptions.add(this.DashboardService.getDashboard()
+      .subscribe((dashboard) => {
+        this.dashboard = dashboard;
+
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 
 }
